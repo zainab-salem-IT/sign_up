e5aa3ffd8232fac0d521224200f4d693cfae0983
zainab-salem-IT <116463477+zainab-salem-IT@users.noreply.github.com>
2022-11-08 23:12:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sign_up/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sign_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
